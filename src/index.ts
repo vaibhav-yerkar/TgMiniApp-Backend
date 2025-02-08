aef7fffe65368fe6715defee891cfedd91fc1dea
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2025-02-08 11:53:40 +0530

    added error handling

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import cron from "node-cron";
 import cors from "cors";
 import { swaggerSpec } from "./config/swagger";
 import { resetDailyTasks } from "./config/cron-work";
-import { errorHandler } from "./middleware/errorHandler";
 
 import adminRouter from "./router/adminRouter";
 import taskRouter from "./router/taskRouter";
@@ -24,13 +23,6 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-process.on("uncaughtException", (error) => {
-  console.error("Uncaught Exception:", error);
-});
-process.on("unhandledRejection", (error) => {
-  console.error("Unhandled Rejection:", error);
-});
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
@@ -40,8 +32,6 @@ app.use("/auth", authRouter);
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 app.use("/anmt", announcemetRouter);
-
-app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
