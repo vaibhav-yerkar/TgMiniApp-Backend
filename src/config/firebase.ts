138c4aa0a7679a6abfe27a5a3e53a84339b2cffa
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2025-02-25 17:48:11 +0530

    added firebase notifications

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,8 @@
 import admin from "firebase-admin";
 import { ServiceAccount } from "firebase-admin";
 
-const serviceAccount: ServiceAccount = JSON.parse(
-  process.env.FIREBASE_ADMIN_CREDENTIALS as string
-);
+// const serviceAccount: ServiceAccount = require("./firebase-admin.json");
+const serviceAccount: ServiceAccount = require("/etc/secrets/firebase-admin.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
