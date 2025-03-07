ade524a17bb82e18c6029756ab6433692ebbc2b7
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2025-03-07 09:43:58 +0530

    refactoring tg-bot

--- src/service/telegramBotService.ts
+++ src/service/telegramBotService.ts
@@ -171,7 +171,7 @@ export const initlialiseTelegramBot = async (app?: express.Express) => {
       return;
     }
 
-    if (text.includes("/start ")) {
+    if (text.includes("/start")) {
       try {
         const isPrivateChat = msg.chat.type === "private";
 
@@ -231,7 +231,7 @@ export const initlialiseTelegramBot = async (app?: express.Express) => {
     }
 
     // Command: /leaderboard
-    if (text.includes("/leaderboard ")) {
+    if (text.includes("/leaderboard")) {
       if (userId) {
         const leaderboardText = await formatLeaderBoard(userId);
         bot.sendMessage(chatId, leaderboardText, { parse_mode: "Markdown" });
@@ -242,7 +242,7 @@ export const initlialiseTelegramBot = async (app?: express.Express) => {
     }
 
     // Command: /task or /tasks
-    if (text.includes("/task ")) {
+    if (text.includes("/task")) {
       const tasksText = await getRecentTasks();
       bot.sendMessage(chatId, tasksText, { parse_mode: "Markdown" });
       return;
